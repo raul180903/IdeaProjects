21824462205710da56e5d7a450d391fef10e174e
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-03-26 20:29:57 -0300

    Initial commit

---
+++ MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,15 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        var meuNome = "Raul";
+        var nomeNamorada = "Ellen";
+        var nomeFilho = "Ravi";
+        var nomeFilha = "Eloá";
+
+        System.out.println("Boa noite, me chamo " + meuNome);
+        System.out.println("Estou prestes a ficar noivo do amor da mimha vida, a " + nomeNamorada);
+        System.out.println("Nós queremos um casal de pretinhos, nosso princípe " + nomeFilho);
+        System.out.println("E nossa princesinha " + nomeFilha);
+    }
+
+}
---
+++ MeuPrimeiroProjeto/src/MinhasContas.java
@@ -0,0 +1,14 @@
+public class MinhasContas {
+
+    public static void main(String[]args){
+        operacoes(10 , 10);
+    }
+
+    static void operacoes(int x, int y){
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+    }
+}
