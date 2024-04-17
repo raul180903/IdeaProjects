e40824056559a6f87bc34a15c89bf5563f534636
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-04-16 21:03:42 -0300

    integer = (int)!!

---
+++ MegaSena-Loops/src/Primitivos.java
@@ -0,0 +1,11 @@
+import com.sun.source.doctree.SystemPropertyTree;
+
+public class Primitivos {
+    public static void main (String[] args){
+        int x = 2;  //APENAS ALOCAR ESPAÇO COM NUMEROS INTEIROS PRIMITIVOS
+        Integer y = 4;  //  CASO FAZER UMA OPERAÇÃO COM ESSE TIPO PRIMITIVO MELHOR USAR = INTEGER
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
